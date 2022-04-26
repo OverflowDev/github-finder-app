f1f392e951a48dba2821f338e4996b330d1f2fa1
unknown <OverflowDev>
2022-04-26 14:11:00 +0100

    Axios not working

--- src/context/github/GithubReducer.js
+++ src/context/github/GithubReducer.js
@@ -9,25 +9,25 @@ const githubReducer = (state, action) => {
                 loading: false,
             }
 
-        case 'GET_USER_AND_REPOS':
+        // case 'GET_USER_AND_REPOS':
+        //     return {
+        //         ...state, 
+        //         user: action.payload.user,
+        //         repos: action.payload.repos,
+        //         loading: false,
+        //     }
+        case 'GET_USER':
             return {
                 ...state, 
-                user: action.payload.user,
-                repos: action.payload.repos,
+                user: action.payload,
                 loading: false,
             }
-        // case 'GET_USER':
-        //     return {
-        //         ...state, 
-        //         user: action.payload,
-        //         loading: false,
-        //     }
-        // case 'GET_REPOS':
-        //     return {
-        //         ...state, 
-        //         repos: action.payload,
-        //         loading: false,
-        //     }
+        case 'GET_REPOS':
+            return {
+                ...state, 
+                repos: action.payload,
+                loading: false,
+            }
 
         case 'SET_LOADING':
             return {
--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -4,7 +4,7 @@ import {useParams, Link} from 'react-router-dom'
 import GithubContext from '../context/github/GithubContext'
 import Spinner from '../components/layouts/Spinner'
 import RepoList from '../components/repos/RepoList'
-import {getUserAndRepos} from '../context/github/GithubActions'
+import {getUser, getUserRepos} from '../context/github/GithubActions'
 
 function User() {
 
@@ -16,13 +16,13 @@ function User() {
       dispatch({type: 'SET_LOADING'})
 
       const getUserData = async() => {
-        // const userData = await getUser(params.login)
-        // dispatch({type: 'GET_USER', payload: userData})
-        // const userRepoData = await getUserRepos(params.login)
-        // dispatch({type: 'GET_REPOS', payload: userRepoData})
+        const userData = await getUser(params.login)
+        dispatch({type: 'GET_USER', payload: userData})
+        const userRepoData = await getUserRepos(params.login)
+        dispatch({type: 'GET_REPOS', payload: userRepoData})
 
-        const userData = await getUserAndRepos(params.login)
-        dispatch({type: 'GET_USER_AND_REPOS', payload: userData})
+        // const userData = await getUserAndRepos(params.login)
+        // dispatch({type: 'GET_USER_AND_REPOS', payload: userData})
       }
 
       getUserData()
